d2709354a4ff15df05f9350a4dcd6f9c30f5ceae
ragnarlodbrok1992 <maraus92@gmail.com>
2025-01-17 16:29:44 +0100

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -510,6 +510,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
   // Main loop
   MSG msg = {};
+  m_scissorRect = { 0, 0, static_cast<LONG>(m_width), static_cast<LONG>(m_height) };
 
   while (main_loop) {
     // Windows messages thing
